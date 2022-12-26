a6577947e7ab92ba14f6b20854e0607305b63c09
akram-kamil <akrambean@gmail.com>
2022-12-27 00:03:25 +0300

    make the first page with the base and featured screen

---
+++ lib/screens/featured_screen.dart
@@ -0,0 +1,100 @@
+import 'package:edu_app/constants/color.dart';
+import 'package:edu_app/widgets/circle_botton.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import '../widgets/search_testfield.dart';
+
+class FeaturedScreen extends StatefulWidget {
+  const FeaturedScreen({super.key});
+
+  @override
+  State<FeaturedScreen> createState() => _FeaturedScreenState();
+}
+
+class _FeaturedScreenState extends State<FeaturedScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.light,
+        child: Scaffold(
+          body: Column(
+            children: const [
+              AppBar(),
+              Body(),
+            ],
+          ),
+        ));
+  }
+}
+
+class Body extends StatelessWidget {
+  const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(top: 10, left: 20, right: 20),
+          child:
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+            Text(
+              "Explore Categories",
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
+            TextButton(
+                onPressed: () {},
+                child: Text(
+                  "See All",
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyMedium
+                      ?.copyWith(color: kPrimaryColor),
+                ))
+          ]),
+        )
+      ],
+    );
+  }
+}
+
+class AppBar extends StatelessWidget {
+  const AppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(top: 50, left: 20, right: 20),
+      height: 200,
+      width: double.infinity,
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(20), bottomRight: Radius.circular(20)),
+        gradient: LinearGradient(
+            colors: [Color(0xff887ff2), Color(0xff6849ef)],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight),
+      ),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Hello, Admin \nGood Morning",
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+              CircleButton(icon: Icons.notifications, onPressed: () {})
+            ],
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const SearchTextField(),
+        ],
+      ),
+    );
+  }
+}
